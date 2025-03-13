4a62622ba486adbe6a780d434b447b92e7f9ada2
martin.solihin <martinchristiansolihin@gmail.com>
2025-03-13 15:18:52 +0100

    Fix: Desktop Character setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,37 @@
 using UnityEngine;
+using UnityEngine.XR.Management;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject xrOrigin;
+    public GameObject desktopCharacter;
+
+    private void Start()
+    {
+        var xrSettings = XRGeneralSettings.Instance;
+        if (xrSettings == null)
+        {
+            Debug.Log("XRSettings is null");
+            return;
+        }
+
+        var xrManager = xrSettings.Manager;
+        if (xrManager == null)
+        {
+            Debug.Log("XRManager is null");
+            return;
+        }
+
+        var xrLoader = xrManager.activeLoader;
+        if (xrLoader == null)
+        {
+            Debug.Log("XRLoader is null");
+            xrOrigin.SetActive(false);
+            desktopCharacter.SetActive(true);
+            return;
+        }
+
+        xrOrigin.SetActive(true);
+        desktopCharacter.SetActive(false);
+    }
 }
