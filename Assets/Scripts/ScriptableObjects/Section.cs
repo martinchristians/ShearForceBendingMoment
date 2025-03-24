7da42215388c7417672737422e48b0008c08c27f
martin.solihin <martinchristiansolihin@gmail.com>
2025-03-24 01:21:22 +0100

    Add: Prefab hint, measurement, task

--- Assets/Scripts/ScriptableObjects/Section.cs
+++ Assets/Scripts/ScriptableObjects/Section.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Section", menuName = "Exercise/Section")]
 public class Section : ScriptableObject
 {
+    [SerializeField] private string title;
+
     public List<Task> tasks = new();
 }
