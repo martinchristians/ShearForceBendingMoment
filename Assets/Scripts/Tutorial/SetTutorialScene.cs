664eee84e871bf036ff7d2cdaa7b6eef016f1e7c
martin.solihin <martinchristiansolihin@gmail.com>
2025-04-01 01:40:43 +0200

    Add: Sequence 2

--- Assets/Scripts/Tutorial/SetTutorialScene.cs
+++ Assets/Scripts/Tutorial/SetTutorialScene.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class SetTutorialScene : MonoBehaviour
 {
     private TutorialDataManager _manager;
-    [SerializeField] private bool _hasAudioEverFinished;
+    private bool _hasAudioEverFinished;
 
     public AudioSource audioSource;
+    [SerializeField] private AttachableContainer[] attachableContainers;
+
     public List<TriggerAction> onStart;
 
     private void Awake()
@@ -23,6 +25,7 @@ public class SetTutorialScene : MonoBehaviour
     {
         _hasAudioEverFinished = _manager.navigations[_manager.currentActive].isDone;
         _manager.CheckFirstTime();
+        ResetAttachableObjectInsideContainer();
     }
 
     private void Update()
@@ -36,6 +39,25 @@ public class SetTutorialScene : MonoBehaviour
             _hasAudioEverFinished = true;
             _manager.navigations[_manager.currentActive].isDone = true;
             _manager.CheckFirstTime();
+        }
+    }
+
+    private void ResetAttachableObjectInsideContainer()
+    {
+        foreach (var ac in attachableContainers)
+        {
+            foreach (var ao in ac.attachedObjectInsideCollider)
+            {
+                ao.attachableContainer = null;
+                ao.attachableContainers.Clear();
+                ao.transform.position = new Vector3(0, -5, 0);
+
+                var rb = ao.GetComponent<Rigidbody>();
+                rb.useGravity = true;
+                rb.isKinematic = false;
+            }
+
+            ac.attachedObjectInsideCollider.Clear();
         }
     }
 }
